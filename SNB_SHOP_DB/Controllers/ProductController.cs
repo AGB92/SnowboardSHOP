905d6c1321066f39aafaec85b5c1246476c4b139
AGB92 <adam.geborek@gmail.com>
2024-05-25 22:06:21 +0200

    commit f2 57m

---
+++ SNB_SHOP_DB/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SNB_SHOP_DB.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
