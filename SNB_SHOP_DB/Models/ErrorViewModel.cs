ebb5cef413ea1dc264f15fbe8899b99f321dddbe
AGB92 <adam.geborek@gmail.com>
2024-05-25 22:03:37 +0200

    aż do dodania ProductModel

---
+++ SNB_SHOP_DB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SNB_SHOP_DB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ SNB_SHOP_DB/Models/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿namespace SNB_SHOP_DB.Models
+{
+    public class ProductModel
+    {
+        public int ID { get; set; }
+        public string Product_Type1 { get; set; }
+        public string Product_Type2 { get; set; }
+        public string Sex { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Size { get; set; }
+        public int Stock { get; set; }
+        public double Price { get; set; }
+    }
+}
