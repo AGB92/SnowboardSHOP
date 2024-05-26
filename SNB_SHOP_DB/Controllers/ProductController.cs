ac74e8442cb0aba565d178a3485f3dc2ba1bc05b
AGB92 <adam.geborek@gmail.com>
2024-05-26 02:09:55 +0200

    ogarnięta funkcjonalność

--- SNB_SHOP_DB/Controllers/ProductController.cs
+++ SNB_SHOP_DB/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SNB_SHOP_DB.Models;
 using SNB_SHOP_DB.Repository;
+using System.CodeDom;
 
 namespace SNB_SHOP_DB.Controllers
 {
@@ -14,20 +15,82 @@ namespace SNB_SHOP_DB.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var product = new ProductModel()
-            {
-                Product_Type1 = "Odzież",
-                Product_Type2="Spodnie",
-                Sex="F",
-                Brand="Nitro",
-                Model="M1",
-                Size="M",
-                Stock=4,
-                Price=1099.99
-            };
+            var productList =await _manager.GetProducts();
+            return View(productList);
+        }
 
-            await _manager.AddProductAsync(product);
+        [HttpGet]
+        public IActionResult Add() 
+        {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add (ProductModel product)
+        {
+            try 
+            {
+                await _manager.AddProductAsync(product);
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                return View(product);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Remove(int id) 
+        {
+            var productToDelete =await _manager.GetProduct(id);
+            if(productToDelete != null) 
+            {
+				return View(productToDelete);
+			}
+            else 
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RemoveConfirm(int id)
+        {
+            try 
+            {
+                await _manager.RemoveProduct(id);
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                return RedirectToAction("Remove", id);
+            }
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var filmToEdit = await _manager.GetProduct(id);
+            if(filmToEdit != null)
+            {
+				return View(filmToEdit);
+			}
+            else 
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(ProductModel product) 
+        {
+            await _manager.UpdateProduct(product);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            var product=await _manager.GetProduct(id);
+            return View(product);
         }
     }
 }
--- SNB_SHOP_DB/Repository/ProductManager.cs
+++ SNB_SHOP_DB/Repository/ProductManager.cs
@@ -1,4 +1,6 @@
 ﻿using System.IO;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
 using SNB_SHOP_DB.Data;
 using SNB_SHOP_DB.Models;
 
@@ -14,34 +16,57 @@ namespace SNB_SHOP_DB.Repository
         }
         public async Task<bool> AddProductAsync(ProductModel productModel)
         {
-            await _context.AddAsync(productModel);
-            await _context.SaveChangesAsync();
+            try 
+            {
+                await _context.AddAsync(productModel);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                productModel.ID = 0;
+                await _context.SaveChangesAsync();
+            }
             return true;
         }
 
-        public ProductManager RemoveProduct(int id)
+        public async Task<ProductManager> RemoveProduct(int id)
         {
+            var productToDelete=await GetProduct(id);
+            if (productToDelete != null) 
+            {
+                 _context.Products.Remove(productToDelete);
+                await _context.SaveChangesAsync();
+            }
             return this;
         }
 
-        public ProductManager UpdateProduct(ProductModel productModel)
+        public async Task<ProductManager> UpdateProduct(ProductModel productModel)
         {
+            _context.Products.Update(productModel);
+            await _context.SaveChangesAsync();
             return this;
-        }
-        //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-        public ProductManager ChangeTitle(int id, string newTitle)
-        {
-            return this;
-        }
-        //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-        public ProductManager GetProduct(int id)
-        {
-            return null;
         }
 
-        public List<ProductModel> GetProducts()
+        public async Task<ProductManager> ChangeProductType1(int id, string newPType1)
         {
-            return null;
+            var productToChangeType1 = await GetProduct(id);
+            if (productToChangeType1 != null) 
+            {
+                productToChangeType1.Product_Type1= newPType1;
+                await _context.SaveChangesAsync();
+            }
+            return this;
+        }
+
+        public async Task<ProductModel> GetProduct(int id)
+        {
+            return await _context.Products.SingleOrDefaultAsync(p=>p.ID == id);
+        }
+
+        public async Task<List<ProductModel>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return products;
         }
     }
 }
